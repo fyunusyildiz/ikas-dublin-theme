3d14220951970a076546be34e890ebd105e923e5
Fikret Yunus Yildiz <fyunusyildiz@gmail.com>
2024-09-07 00:16:27 +0300

    mail active account

--- src/components/__generated__/pages/account/activate.ts
+++ src/components/__generated__/pages/account/activate.ts
@@ -1,6 +1,8 @@
+import Component0 from "src/components/header";
+import Component1 from "src/components/footer";
 
   const Components = {
-    
+    "06d23529-6a32-4794-aad9-2bcc1a4f4952": Component0,"1e9eb5dc-c0a7-4bbb-a41d-f311be80dd16": Component1
   };
   
 export default Components;
