fba5ee2c16647c6dc84157ca5771a6310dc89c30
Fikret Yunus Yildiz <fyunusyildiz@gmail.com>
2024-09-14 00:16:34 +0300

    add footer to login and register pages

--- src/components/__generated__/pages/account/login.ts
+++ src/components/__generated__/pages/account/login.ts
@@ -1,8 +1,9 @@
 import Component0 from "src/components/header";
-import Component1 from "src/components/login";
+import Component1 from "src/components/footer";
+import Component2 from "src/components/login";
 
   const Components = {
-    "06d23529-6a32-4794-aad9-2bcc1a4f4952": Component0,"92f51797-f71d-4593-9f4d-15e7f56db072": Component1
+    "06d23529-6a32-4794-aad9-2bcc1a4f4952": Component0,"1e9eb5dc-c0a7-4bbb-a41d-f311be80dd16": Component1,"92f51797-f71d-4593-9f4d-15e7f56db072": Component2
   };
   
 export default Components;
