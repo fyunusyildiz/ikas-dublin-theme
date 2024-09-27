ace9f98e84ee93087405005ace264cc9be9607b8
Fikret Yunus Yildiz <fyunusyildiz@gmail.com>
2024-09-28 00:08:56 +0300

    build fix

--- src/components/__generated__/pages/index.ts
+++ src/components/__generated__/pages/index.ts
@@ -1,16 +1,10 @@
-import Component0 from "src/components/title";
-import Component1 from "src/components/slider";
-import Component2 from "src/components/category-banner-with-two";
-import Component3 from "src/components/category-banner-with-three";
-import Component4 from "src/components/category-banner-with-four";
-import Component5 from "src/components/header";
-import Component6 from "src/components/footer";
-import Component7 from "src/components/category-banner-with-five";
-import Component8 from "src/components/full-banner";
-import Component9 from "src/components/about-home";
+import Component0 from "src/components/slider";
+import Component1 from "src/components/header";
+import Component2 from "src/components/footer";
+import Component3 from "src/components/full-banner";
 
   const Components = {
-    "471dfb8e-49ae-4974-aa19-8029a5671040": Component0,"e26b43b6-b0fe-40af-a56a-9455fd0b6700": Component1,"5841dec2-136f-4c75-a3cf-6a38ac628787": Component2,"0c47c4af-6036-465b-9491-f55ad7efb527": Component3,"8624b237-ba5f-4e4c-9d0e-36cdc1a1667e": Component4,"06d23529-6a32-4794-aad9-2bcc1a4f4952": Component5,"1e9eb5dc-c0a7-4bbb-a41d-f311be80dd16": Component6,"4e335a89-9660-4276-862c-6ac7155cf933": Component7,"b1f39f3c-bc3b-4125-b785-b2fd6ff48952": Component8,"aea91a57-8f54-4c2c-b8be-c4b1a635738f": Component9
+    "e26b43b6-b0fe-40af-a56a-9455fd0b6700": Component0,"06d23529-6a32-4794-aad9-2bcc1a4f4952": Component1,"1e9eb5dc-c0a7-4bbb-a41d-f311be80dd16": Component2,"b1f39f3c-bc3b-4125-b785-b2fd6ff48952": Component3
   };
   
 export default Components;
