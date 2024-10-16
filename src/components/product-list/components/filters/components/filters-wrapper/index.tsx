3106c1a5fc7cfe1c46511e54a7611cb95fc5220a
Fikret Yunus Yildiz <fyunusyildiz@gmail.com>
2024-10-16 16:01:42 +0300

    responsive fix

--- src/components/product-list/components/filters/components/filters-wrapper/index.tsx
+++ src/components/product-list/components/filters/components/filters-wrapper/index.tsx
@@ -10,7 +10,7 @@ type FiltersWrapperProps = {
 export const FiltersWrapper = (props: FiltersWrapperProps) => {
   return (
     <div
-      className={`flex col-span-1 sm:col-span-4 flex-col h-fit gap-3 header-sm:gap-0 header-sm:mb-10 header-sm:last:mb-0 header-sm:last:border-b-0 border-b border-x border-solid border-[#222] header-sm:border-x-0`}
+      className={`flex col-span-1 h-full sm:col-span-4 flex-col h-fit gap-3 header-sm:gap-0 header-sm:mb-10 header-sm:last:mb-0 header-sm:last:border-b-0 border-b border-x border-solid border-[#222] header-sm:border-x-0`}
     >
       <p className="text-2xs px-6 header-sm:px-5 header-sm:py-2 py-3 border-b header-sm:border-y border-solid border-[#222] uppercase">
         {props.title}
